ce0eafa3568a6682bdf7c403297eebb52be0ffeb
YounesDEHBI1 <younesdehbi.n1@gmail.com>
2020-11-16 12:36:49 +0100

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+// Load new data to display
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("demo").innerHTML =
+                this.responseText;
+        }
+    };
+    xhttp.open("GET", "assets/files/ajax_info.txt", true);
+    xhttp.send();
+}
